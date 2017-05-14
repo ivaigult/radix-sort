a2f8e555a91e4da2f0dfef48c912857f9e371779
ivaigult <i.vaigult@gmail.com>
2017-05-14 18:25:42 +0300

    Initial radix sort implementation

--- radix-sort.cpp
+++ radix-sort.cpp
@@ -5,6 +5,8 @@
 #include <cassert>
 #include <cstdlib>
 
+#include "radix-sort.hpp"
+
 int main(int argc, char** argv) {
     int size = 0;
     std::cin >> size;
@@ -14,7 +16,7 @@ int main(int argc, char** argv) {
     std::istream_iterator<int> input(std::cin);
     std::copy(input, std::istream_iterator<int>(), v.begin());
 
-    std::sort(v.begin(), v.end());
+    radix_sort(v.begin(), v.end());
     
     std::cout << size << std::endl;
     std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
@@ -22,4 +24,3 @@ int main(int argc, char** argv) {
     
     exit(EXIT_SUCCESS);
 }
-
---
+++ radix-sort.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <iterator>
+#include <limits>
+#include <cstdint>
+#include <array>
+
+namespace detail {
+
+template<typename value_t, typename radix_t = uint8_t>
+struct radix_sort_helper {
+    typedef value_t value_type;
+    typedef radix_t radix_type;
+    enum { num_digits = sizeof(value_type) / sizeof(radix_type) };
+    
+    typedef std::vector<value_t> bucket_type;
+    typedef std::array<bucket_type, std::numeric_limits<radix_type>::max()> array_type;
+
+    static radix_type digit(size_t num, value_type value) {
+        const radix_type mask = std::numeric_limits<radix_type>::max();
+        const size_t bit_shift = sizeof(radix_type) * 8 * num;
+        return mask & (value << bit_shift);
+    }
+};
+
+}
+
+template<typename iterator_t>
+void radix_sort(iterator_t begin, iterator_t end) {
+    typedef typename std::iterator_traits<iterator_t>::value_type value_type;
+    typedef detail::radix_sort_helper<value_type> helper_type;
+    
+    typename helper_type::array_type array;
+    
+    for(size_t ii = 0; ii < helper_type::num_digits; ++ii) {
+        for(iterator_t it = begin; it != end; ++it) {
+            array[helper_type::digit(ii, *it)].push_back(*it);
+        }
+
+        iterator_t it = begin;
+        for(typename helper_type::bucket_type& bucket : array) {
+            for(const value_type& value: bucket) {
+                *(it++) = value;
+            }
+            bucket.clear();
+        }
+    }
+}
