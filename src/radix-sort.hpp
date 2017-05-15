27b74785a696bb3a3128e584bfd5d7a7637dabaa
Ivan <i.vaigult@gmail.com>
2017-05-15 03:30:26 -0700

    m0ar aliases

--- src/radix-sort.hpp
+++ src/radix-sort.hpp
@@ -11,36 +11,41 @@ namespace detail {
 
 template<typename value_t>
 struct bucket_node {
+    typedef value_t value_type;
     value_t               value;
     bucket_node<value_t>* tail;
 };
 
 template<typename value_t>
 struct bucket_storage {
+    typedef bucket_node<value_t> node_type;
+
     bucket_storage(size_t num_elements)
         : _num_allocated(0)
         , _data(num_elements)
     {}
 
-    bucket_node<value_t>* alloc() { return &_data[_num_allocated++]; }
-    void                  reset() { _num_allocated = 0; }
+    node_type* alloc() { return &_data[_num_allocated++]; }
+    void       reset() { _num_allocated = 0; }
 private:
-    size_t                             _num_allocated;
-    std::vector<bucket_node<value_t> > _data;
+    size_t                 _num_allocated;
+    std::vector<node_type> _data;
 };
     
 template<typename value_t>
 struct bucket {
-    typedef value_t value_type;
+    typedef value_t                 value_type;
+    typedef bucket_storage<value_t> storage_type;
+    typedef bucket_node<value_t>    node_type;
 
-    bucket(bucket_storage<value_t>& s)
+    bucket(storage_type& s)
         : _storage(s)
         , _front(nullptr)
         , _back(nullptr)
     {}
     
     void push_back(value_type v) {
-        bucket_node<value_type>* new_node = _storage.alloc();
+        node_type* new_node = _storage.alloc();
         new_node->value = v;
         new_node->tail = nullptr;
         if (!_front) {
@@ -58,28 +63,28 @@ struct bucket {
     }
 
     struct iterator {
-        iterator(bucket_node<value_type>* node) : _node(node) {}
+        iterator(node_type* node) : _node(node) {}
         iterator&    operator=(const iterator& it) { _node = it._node; return *this; }
         bool         operator==(const iterator& it) const { return _node == it._node; }
         bool         operator!=(const iterator& it) const { return _node != it._node; }
-        iterator&    operator++()    { _node = static_cast<bucket_node<value_type>*>(_node->tail); return *this; }
+        iterator&    operator++()    { _node = static_cast<node_type*>(_node->tail); return *this; }
         iterator     operator++(int) {
-            bucket_node<value_type>* prev_node = _node;
-            _node = static_cast<bucket_node<value_type>*>(_node->tail);
+            node_type* prev_node = _node;
+            _node = static_cast<node_type*>(_node->tail);
             return prev_node;
         }
         value_type   operator*() const { return _node->value; }
     private:
-        bucket_node<value_type>* _node;
+        node_type* _node;
     };
 
     iterator begin() { return _front; }
     iterator end()   { return nullptr; }
     
 private:
-    bucket_storage<value_t>& _storage;
-    bucket_node<value_type>* _front;
-    bucket_node<value_type>* _back;
+    storage_type& _storage;
+    node_type* _front;
+    node_type* _back;
 };
     
 template<typename value_t, typename radix_t = uint8_t>
@@ -90,6 +95,10 @@ struct radix_sort_helper {
     enum { bits_per_digit = sizeof(radix_type) * 8                  };
     enum { max_digit      = std::numeric_limits<radix_type>::max()  };
     enum { num_buckets    = max_digit + 1                           };
+
+    typedef bucket_storage<value_t>   storage_type;
+    typedef bucket<value_t>           bucket_type;
+    typedef std::vector<bucket_type>  bucket_vec_type;
 
     static radix_type digit(size_t num, value_type value) {
         const size_t bit_shift = bits_per_digit * num;
@@ -104,9 +113,11 @@ void radix_sort(iterator_t begin, iterator_t end) {
     typedef typename std::iterator_traits<iterator_t>::value_type value_type;
     typedef detail::radix_sort_helper<value_type> helper_type;
     
-    size_t num_elements = std::distance(begin, end);
-    detail::bucket_storage<value_type> storage(num_elements);
-    std::vector<detail::bucket<value_type> > array(helper_type::num_buckets, detail::bucket<value_type>(storage));
+    assert(begin <= end);
+    size_t num_elements = static_cast<size_t>(std::distance(begin, end));
+
+    typename helper_type::storage_type    storage(num_elements);
+    typename helper_type::bucket_vec_type array(helper_type::num_buckets, typename helper_type::bucket_type(storage));
     
     for(size_t ii = 0; ii < helper_type::num_digits; ++ii) {
         for(iterator_t it = begin; it != end; ++it) {
@@ -114,7 +125,7 @@ void radix_sort(iterator_t begin, iterator_t end) {
         }
 
         iterator_t it = begin;
-        for(auto& bucket : array) {
+        for(typename helper_type::bucket_type& bucket : array) {
             for(const value_type& value: bucket) {
                 *(it++) = value;
             }
