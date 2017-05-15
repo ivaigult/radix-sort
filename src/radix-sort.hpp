c6c5cad4386878587fe4aa420804f15c4d559556
Ivan <i.vaigult@gmail.com>
2017-05-15 03:48:52 -0700

    Use constexpr

--- src/radix-sort.hpp
+++ src/radix-sort.hpp
@@ -91,10 +91,10 @@ template<typename value_t, typename radix_t = uint8_t>
 struct radix_sort_helper {
     typedef value_t value_type;
     typedef radix_t radix_type;
-    enum { num_digits     = sizeof(value_type) / sizeof(radix_type) };
-    enum { bits_per_digit = sizeof(radix_type) * 8                  };
-    enum { max_digit      = std::numeric_limits<radix_type>::max()  };
-    enum { num_buckets    = max_digit + 1                           };
+    static constexpr size_t     num_digits     = sizeof(value_type) / sizeof(radix_type);
+    static constexpr size_t     bits_per_digit = sizeof(radix_type) * 8;
+    static constexpr radix_type max_digit      = std::numeric_limits<radix_type>::max();
+    static constexpr size_t     num_buckets    = max_digit + 1;
 
     typedef bucket_storage<value_t>   storage_type;
     typedef bucket<value_t>           bucket_type;
