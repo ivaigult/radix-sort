8f99d314155d5717d6b046ea1a81fb91cb97c55d
ivaigult <i.vaigult@gmail.com>
2017-05-18 21:34:37 +0300

    Fix typo

--- include/no_tbb/no_tbb.hpp
+++ include/no_tbb/no_tbb.hpp
@@ -30,7 +30,7 @@ void set_affinity(std::thread& thread, size_t core_index) {
 void set_affinity(std::thread& thread, size_t core_index) {
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
-    CPU_SET(core_index, cpuset);
+    CPU_SET(core_index, &cpuset);
     pthread_setaffinity_np(thread.native_handle(), sizeof(cpu_set_t), &cpuset);
 }
 #else
